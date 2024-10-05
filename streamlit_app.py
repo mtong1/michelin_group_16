5bc94dbf6d1813af0d525df58cdcf170c0db1793
cnrbd <puong23@ssis.edu.vn>
2024-10-05 13:51:18 -0400

    test if api is needed

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,23 +12,23 @@ st.title('🦜🔗 Quickstart App')
 groq_api_key = st.sidebar.text_input('Groq API Key')
 
 def generate_response(input_text):
-    if groq_api_key:
-        os.environ["GROQ_API_KEY"] = groq_api_key
-        llm = ChatGroq(
-            model="llama-3.1-70b-versatile",
-            temperature=0,
-            max_tokens=None,
-            timeout=None,
-            max_retries=2
-        )
-        # Call the model and display the result
-        response = llm([HumanMessage(content=input_text)])
-        # print (type(response))
-        st.info((response.content))
-        st.info(type(response))
+    # if groq_api_key:
+    os.environ["GROQ_API_KEY"] = groq_api_key
+    llm = ChatGroq(
+        model="llama-3.1-70b-versatile",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2
+    )
+    # Call the model and display the result
+    response = llm([HumanMessage(content=input_text)])
+    # print (type(response))
+    st.info((response.content))
+    st.info(type(response))
 
-    else:
-        st.warning('Please enter your Groq API key!', icon='⚠')
+    # else:
+    #     st.warning('Please enter your Groq API key!', icon='⚠')
 
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
